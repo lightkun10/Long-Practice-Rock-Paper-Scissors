9dc20e5b12fe415736aa1c5485143dcadda0ef2b
pandu <lifeisubw@gmail.com>
2024-01-30 20:50:41 +0700

    first commit

--- game.js
+++ game.js
@@ -22,69 +22,117 @@ let losses = 0;
 let ties = 0;
 
 /* DO NOT CHANGE THE CODE ABOVE */
+let coins = 0;
 
 /***************************** HELPER FUNCTIONS ******************************/
 function printHelp() {
-  // Your code here
+  const commands = [
+    "  Type 'r' for Rock",
+    "  Type 'p' for Paper",
+    "  Type 's' for Scissors",
+    "  Type 'q' to quit",
+    "  Type 'h' for a list of valid commands\n"
+  ]
+  commands.forEach(command => console.log(command));
 }
 
 function getWinner(move1, move2) {
-  // Your code here
+  let move1WinsAgainst = VALID_MOVES[move1].winsAgainst;
+  let move2WinsAgainst = VALID_MOVES[move2].winsAgainst;
+
+  if (move1WinsAgainst === move2) {
+    return 1; // move1 beats move2
+  } else if (move2WinsAgainst === move1) {
+    return -1; // move2 beats move1
+  } else {
+    return 0;
+  }
 }
 
 function getCPUMove() {
-  // Your code here
+  const validMoveKeys = Object.keys(VALID_MOVES);
+  const randomIndex = Math.floor(Math.random() * validMoveKeys.length);
+  return validMoveKeys[randomIndex];
 }
 
 function processMove(cmd, cpu) {
-  // Your code here
+  console.log(`You pick ${cmd}, computer picks ${cpu}.`);
+
+  let turnResult = getWinner(cmd, cpu);
+
+  if (turnResult === 1) {
+    console.log("You win!\n");
+    wins++;
+  } else if (turnResult === -1) {
+    console.log("You lose...\n");
+    losses++;
+  } else if (turnResult === 0) {
+    console.log("You tie.\n");
+    ties++;
+  }
 }
 
 /******************************* MAIN FUNCTION *******************************/
 function promptInput(rl) {
   console.log(`${wins} wins - ${losses} losses - ${ties} ties`);
+
   rl.question('> ', (cmd) => {
     cmd = cmd.toLowerCase();
 
+    // Check if coins is zero
+    if (coins === 0) {
+      displayGameOver();
+      rl.close();
+      return;
+    }
+
     if (cmd === 'h') {
       console.log("\nHelp:\n");
-      console.log("  Type 'r' for Rock");
-      console.log("  Type 'p' for Paper");
-      console.log("  Type 's' for Scissors");
-      console.log("  Type 'q' to quit");
-      console.log("  Type 'h' for a list of valid commands\n");
+      printHelp();
     } else if (cmd === 'q') {
       rl.close();
       return;
     } else if (VALID_MOVES[cmd]){
-      const validMoveKeys = Object.keys(VALID_MOVES);
-      const randomIndex = Math.floor(Math.random() * validMoveKeys.length);
-      const cpu = validMoveKeys[randomIndex];
+      let cpu = getCPUMove();
 
-      console.log(`You pick ${cmd}, computer picks ${cpu}.`);
+      processMove(cmd, cpu);
+      coins--;
 
-      if (cmd === cpu) { // tie
-        console.log("You tie.\n");
-        ties++;
-      }
-      else if (VALID_MOVES[cmd].winsAgainst === cpu) { // win
-        console.log("You win!\n");
-        wins++;
-      } else { // loss
-        console.log("You lose...\n");
-        losses++;
+      if (coins === 0) {
+        displayGameOver();
+        rl.close();
+        return;
       }
     } else {
       console.log("\nInvalid command.\n");
-      console.log("  Type 'r' for Rock");
-      console.log("  Type 'p' for Paper");
-      console.log("  Type 's' for Scissors");
-      console.log("  Type 'q' to quit");
-      console.log("  Type 'h' for a list of valid commands\n");
+      printHelp();
     }
 
     promptInput(rl);
   });
+}
+
+function insertCoin(rl) {
+  console.log("Insert coin(s)...");
+  rl.question('> ', (coinInsert) => {
+    coins = Number(coinInsert);
+
+    printHelp();
+    promptInput(rl);
+  })
+}
+
+function displayGameOver() {
+  console.log("\nGame Over!");
+  console.log(`Final Score: ${wins} wins - ${losses} losses - ${ties} ties`);
+
+  if (wins > losses) {
+    console.log("YOU WIN!");
+  } else if (wins < losses) {
+    console.log("YOU LOSE.");
+  } else {
+    console.log("DRAW!");
+  }
 }
 
 /****************************** INITIALIZE GAME ******************************/
@@ -94,13 +142,8 @@ function initializeGame() {
     output: process.stdout
   });
   console.log("Welcome to Rock/Paper/Scissors\n");
-  console.log("  Type 'r' for Rock");
-  console.log("  Type 'p' for Paper");
-  console.log("  Type 's' for Scissors");
-  console.log("  Type 'q' to quit");
-  console.log("  Type 'h' for a list of valid commands\n");
 
-  promptInput(rl);
+  insertCoin(rl);
 }
 
 // start the game if running this file directly, `node game.js`
